e3eda6122c354870ff9ffeaa00f078cb5fd399e0
Bastinoroni96 <gsbasti96@gmail.com>
2024-09-26 22:36:52 +0800

    collision with platform, adjusted jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 300
 @export var gravity = 30
-@export var jump_force = 300
+@export var jump_force = 700
 
 
 func _physics_process(delta):
@@ -11,7 +11,7 @@ func _physics_process(delta):
 		if velocity.y > 1000:
 			velocity.y = 1000
 				
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump_force
 		
 	var horizontal_direction = Input.get_axis("move_left","move_right")
